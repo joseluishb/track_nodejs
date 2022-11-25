20d36dfd1f8453cddac41f67e8f5cc5e3279153e
Jose Luis H <josehb13@gmail.com>
2022-11-25 17:42:56 -0500

    Refactor upload file

--- routes/storage.js
+++ routes/storage.js
@@ -1,22 +1,10 @@
-const multer = require('multer');
 const express = require('express');
 const router = express.Router();
+const uploadMiddleware = require('../utils/handleStorage')
 
 //TODO http://localhost:3100/api/storage
 
-const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    const pathStorage = `${__dirname}/../storage`;
-    cb(null, pathStorage);
-  },
-  filename: function (req, file, cb) {
-    const ext = file.originalname.split(".").pop();
-    const filename = `file-${Date.now()}.${ext}`;
-    cb(null, filename);
-  },
-});
 
-const uploadMiddleware = multer({storage});
 
 
 router.post("/", uploadMiddleware.single("myfile") , (req, res) => {
