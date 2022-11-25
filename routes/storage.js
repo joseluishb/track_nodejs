6085d1f6939e9251aaa267f07ef5526045fce647
Jose Luis H <josehb13@gmail.com>
2022-11-25 17:28:50 -0500

    Upload file

---
+++ routes/storage.js
@@ -0,0 +1,26 @@
+const multer = require('multer');
+const express = require('express');
+const router = express.Router();
+
+//TODO http://localhost:3100/api/storage
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    const pathStorage = `${__dirname}/../storage`;
+    cb(null, pathStorage);
+  },
+  filename: function (req, file, cb) {
+    const ext = file.originalname.split(".").pop();
+    const filename = `file-${Date.now()}.${ext}`;
+    cb(null, filename);
+  },
+});
+
+const uploadMiddleware = multer({storage});
+
+
+router.post("/", uploadMiddleware.single("myfile") , (req, res) => {
+  res.send({a:1});
+});
+
+module.exports = router;
