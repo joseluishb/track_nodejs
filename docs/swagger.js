8d6e62e55ec728879e66c2be01d80e948780e46a
Jose Luis H <josehb13@gmail.com>
2022-11-29 07:45:16 -0500

    Swagger Schemas

--- docs/swagger.js
+++ docs/swagger.js
@@ -10,6 +10,102 @@ const swaggerDefinition = {
     title: "Documentacion de mi API Curso de Node REST",
     version: "1.0.1",
   },
+  servers: [
+    {
+      url: "http://localhost:3001/api",
+    },
+    {
+      url: "https://afternoon-journey-32165.herokuapp.com/api",
+    },
+  ],
+  components: {
+    schemas: {
+      authLogin: {
+        type: "object",
+        required: ["email", "password"],
+        properties: {
+          email: {
+            type: "string",
+          },
+          password: {
+            type: "string",
+          },
+        },
+      },
+      authRegister: {
+        type: "object",
+        required: ["email", "password", "age", "name"],
+        properties: {
+          name: {
+            type: "string",
+          },
+          age: {
+            type: "integer",
+          },
+          email: {
+            type: "string",
+          },
+          password: {
+            type: "string",
+          },
+        },
+      },
+      track: {
+        type: "object",
+        required: ["name", "album", "cover", "artist", "duration", "mediaId"],
+        properties: {
+          name: {
+            type: "string",
+          },
+          album: {
+            type: "string",
+          },
+          cover: {
+            type: "string",
+          },
+          artist: {
+            type: "object",
+            properties: {
+              name: {
+                type: "string",
+              },
+              nickname: {
+                type: "string",
+              },
+              nationality: {
+                type: "string",
+              },
+            },
+          },
+          duration: {
+            type: "object",
+            properties: {
+              start: {
+                type: "integer",
+              },
+              end: {
+                type: "integer",
+              },
+            },
+          },
+          mediaId: {
+            type: "string",
+          },
+        },
+      },
+      storage: {
+        type: "object",
+        properties: {
+          url: {
+            type: "string",
+          },
+          filename: {
+            type: "string",
+          },
+        },
+      },
+    },
+  },
 };
 
 /**
@@ -17,9 +113,7 @@ const swaggerDefinition = {
  */
 const options = {
   swaggerDefinition,
-  apis: [
-          "./routes/*.js"
-        ],
+  apis: ["./routes/*.js"],
 };
 
 const openApiConfigration = swaggerJsdoc(options);
